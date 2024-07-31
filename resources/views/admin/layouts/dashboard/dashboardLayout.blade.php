20e8e4135b7c3cfa76e2412b4fb9813d9ab1a7b0
kaleb <88307946+KalebMolewe23@users.noreply.github.com>
2024-07-31 14:33:39 +0700

    merapikan backend menu

---
+++ resources/views/admin/layouts/dashboard/dashboardLayout.blade.php
@@ -0,0 +1,28 @@
+@include('admin.layouts.header')    
+    <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+        
+        <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+          <div class="app-brand demo">
+            <a href="{{ url('/dashboard') }}" class="app-brand-link">
+              <span class="app-brand-logo demo">
+                <img style="width:130px" src="{{ asset('/assets/image_content/logo.jpg') }}">
+              </span>
+            </a>
+
+            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+              <i class="bx bx-chevron-left bx-sm align-middle"></i>
+            </a>
+          </div>
+
+          <div class="menu-inner-shadow"></div>
+
+            @include('admin.layouts.dashboard.menuDashboard')
+        </aside>
+        <!-- / Menu -->
+
+        <!-- Layout container -->
+        <div class="layout-page">
+          
+        <!-- Navbar -->
+        @include('admin.layouts.navbar')
